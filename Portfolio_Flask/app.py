2b1b223ba9d8fc6514f2dc8aa82064aebe19139c
Baibhaw Kumar Upadhyay <baibhawgamer@gmail.com>
2025-11-21 18:39:30 +0530

    Add files via upload

---
+++ Portfolio_Flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        print("Message received:", name, email, message)
+        return "Form submitted successfully!"
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
